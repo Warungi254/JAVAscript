27ca8b208a629b4e50247ab403b5dcbeb644636d
Carol Waruingi <carolinewaruingu6@gmail.com>
2022-07-04 04:29:45 -0700

    Typeof is used to find out the datatype of a value or a piece of data.It uses the keyword typeof

---
+++ js/typesof operator.js
@@ -0,0 +1,8 @@
+alert(typeof 47);
+alert(typeof 'Samuel');
+alert(typeof "I am a girl");
+alert(typeof ["Allan", 1,2,5,"Paivy"]);
+alert(typeof undefined);
+alert(typeof null);
+alert(typeof true);
+alert({name:"Cynthia"});
