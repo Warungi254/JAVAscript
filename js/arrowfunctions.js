76d2a8b7a7536a12bda3a5076b543bd0a72d7dd4
Carol Waruingi <carolinewaruingu6@gmail.com>
2022-07-04 03:47:38 -0700

    Arrow functions are used to shorten the code

---
+++ js/arrowfunctions.js
@@ -0,0 +1,17 @@
+//var studentList = function(students){
+  //   console.log(students);
+//};
+
+//studentList(["John", "Mary"]);
+
+
+
+
+//when using an arrow function
+
+var studentList =(students) => console.log(students);
+
+studentList(["John", "Mary"]);
+
+
+
